81846017f4ac5a0baeb5d89b7b6ecfd416d11aca
ADzhyhan <a.dzhyhan@gmail.com>
2021-09-24 13:40:38 +0300

    add header on the page. Add some settings, images and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
@@ -17,9 +18,27 @@ function style() {
     .pipe(browserSync.stream());
 }
 
+function html() {
+  return gulp.src('./src/*.html')
+    .pipe(gulp.dest('dist/'))
+}
+
+function font() {
+  return gulp.src('./src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+}
+
+function image() {
+  return gulp.src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+}
 
 async function build() {
   await style();
+  await image();
+  await html();
+  await font();
 }
 
 function watch() {
@@ -30,9 +49,15 @@ function watch() {
     }
   });
   gulp.watch('./src/scss/**/*.scss', style);
+  gulp.watch('./src/img/*', image); 
+  gulp.watch('./src/*.html', html); 
+  gulp.watch('./src/fonts/*', font);
 }
 
 exports.style = style;
+exports.html = html;
+exports.font = font;
+exports.image = image;
 exports.build = build;
 exports.watch = watch;
-exports.default = build;
+exports.default = build; 
